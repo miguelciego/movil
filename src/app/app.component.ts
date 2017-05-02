b173e84a744be628b0d4b1fe7476d8c63e5e88c3
Fernando Castillo <fernando.utepsa@gmail.com>
2017-05-02 14:18:28 -0400

    mejora style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-//import { LoginPage } from '../pages/login/login';
-import { MitabPage } from '../pages/mitab/mitab';
+import { LoginPage } from '../pages/login/login';
+//import { MitabPage } from '../pages/mitab/mitab';
 
 @Component({
   templateUrl: 'app.html'
@@ -10,7 +10,7 @@ import { MitabPage } from '../pages/mitab/mitab';
 })
 export class MyApp {
 
-    rootPage = MitabPage;
+    rootPage = LoginPage;
 
   constructor(platform: Platform) {
      platform.ready().then(() => {
